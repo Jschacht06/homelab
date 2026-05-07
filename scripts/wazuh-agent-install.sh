24c60dc2a7537f03f10a65c9f8763fa1dfc81026
Juha Schacht <juha.schacht@gmail.com>
2026-05-07 13:10:31 +0200

    wazuh-agent-script v1

---
+++ scripts/wazuh-agent-install.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+set -e
+
+
+
+echo "[+] Updating package information..."
+
+apt-get update
+
+
+echo "[+] Installing dependencies..."
+
+apt-get install -y curl gnupg apt-transport-https
+
+
+echo "[+] Installing GPG Keys..."
+
+curl -s https://packages.wazuh.com/key/GPG-KEY-WAZUH | gpg --no-default-keyring --keyring gnupg-ring:/usr/share/keyrings/wazuh.gpg --import && chmod 644 /usr/share/keyrings/wazuh.gpg
+
+
+
+
+
+echo "[+] Adding the repositories & Updating package information..."
+
+echo "deb [signed-by=/usr/share/keyrings/wazuh.gpg] https://packages.wazuh.com/4.x/apt/ stable main" | tee -a /etc/apt/sources.list.d/wazuh.list
+
+apt-get update
+
+
+
+echo "[+] Installing the agent..."
+
+WAZUH_MANAGER="192.168.50.107" apt-get install -y wazuh-agent
+
+
+echo "[+] Enabling and Starting the agent..."
+
+systemctl daemon-reload
+
+systemctl enable wazuh-agent
+
+systemctl start wazuh-agent
+
+
+
+
+echo "[+] Disabling automatic updates..."
+
+echo "wazuh-agent hold" | dpkg --set-selections
+
+
+echo "[+] Done."
